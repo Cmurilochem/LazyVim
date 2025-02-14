42387a2b67106ea1ea934a9ce6402f175a759e2a
Cmurilochem <carlosmurilorocha@gmail.com>
2025-02-14 13:49:19 +0100

    Refactor copilot config

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true, -- Enable inline suggestions
+        auto_trigger = true, -- Automatically trigger suggestions
+        keymap = {
+          accept = "<Tab>", -- Accept suggestion
+          next = "<M-]>", -- Move to next suggestion
+          prev = "<M-[>", -- Move to previous suggestion
+          dismiss = "<C-]>", -- Dismiss suggestion
+        },
+      },
+      panel = { enabled = false }, -- Disable panel if you don't want extra UI
+    })
+  end,
+}
